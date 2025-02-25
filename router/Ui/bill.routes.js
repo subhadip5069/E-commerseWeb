e618e793096ed767d32d48fdf24dde4f07439f9f
subhadip dey <subhadip5069@gmail.com>
2025-02-26 01:49:25 +0530

    bill change

--- router/Ui/bill.routes.js
+++ router/Ui/bill.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const billController = require('../../controller/ui/bill.ui.controller');
-const authMiddleware = require('../../middleware/Auth');
+const {authMiddleware} = require('../../middleware/Auth');
 
 router.post('/pay',authMiddleware,billController.createBill)
 
